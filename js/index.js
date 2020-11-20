332829a848c44fdb055db7dd72703b4e8a62d9c3
killedmyself <markmisra@gmail.com>
2020-11-20 18:52:56 +0200

    Add local storage handling

--- js/index.js
+++ js/index.js
@@ -1,25 +1,39 @@
-const startBtn = document.getElementById("Start")
-const newGameBtn = document.getElementById("NewGame")
-const saveBtn = document.getElementById("Save")
+const startBtn = document.getElementById("Start");
+const newGameBtn = document.getElementById("NewGame");
+const saveBtn = document.getElementById("Save");
 
-const points = document.getElementById("Points")
-const counter = points.querySelector(".counter")
+const points = document.getElementById("Points");
+const counter = points.querySelector(".counter");
+const finalScore = document.getElementById("Score");
 
 const clock = document.getElementById("Timer");
 const minutesSpan = clock.querySelector(".minutes");
 const secondsSpan = clock.querySelector(".seconds");
 
-const overlay = document.getElementById("Overlay")
+const overlay = document.getElementById("Hidden");
+const name = document.getElementById("Name");
+
+const gameField = document.getElementById("GameField");
+
+const myStorage = window.localStorage;
 
 
 let timeinterval;
 let score = 0;
 let isStartGamePressed = false;
+let timeLimitExceeded = false;
+
+
+
 
 const printDefaults = () => {
   minutesSpan.innerHTML = "01"
   secondsSpan.innerHTML = "00";
   counter.innerHTML = "<b>0</b>";
+}
+
+const printScore = (id ,point) => {
+  id.innerHTML = point.toString();
 }
 
 
@@ -53,6 +67,10 @@ function getTimeRemaining(endtime) {
 const initializeClock = (endtime) => {
   const updateClock = () => {
     var t = getTimeRemaining(endtime);
+    if (t.minutes == 0 && t.seconds == 0) {
+      timeLimitExceeded = true;
+      newGame()
+    }
     minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
     secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
   }
@@ -68,10 +86,13 @@ const initializeClock = (endtime) => {
 
 const showForm = () => {
   overlay.style.display = 'grid';
+  printScore(finalScore, score)
 }
 
 const hideForm = () => {
   overlay.style.display = 'none';
+
+  myStorage.setItem(name.value, finalScore.textContent)
 }
 
 
@@ -92,6 +113,8 @@ const newGame = function() {
       return;
   }
   showForm();
+  
+  // keepValues();
   reset();
 }
 
@@ -101,9 +124,17 @@ const newGame = function() {
 
 
 // TODO: create game logic 
+
+const addScore = () => {
+  score++;
+  printScore(counter,score);
+}
+
+
 // TODO: side-bar impl
 
 
-startBtn.addEventListener("click", () => { startGame()});
-newGameBtn.addEventListener("click", () => { newGame()});
-saveBtn.addEventListener("click", () => { hideForm()});
+startBtn.addEventListener("click", () => { startGame() });
+newGameBtn.addEventListener("click", () => { newGame() });
+saveBtn.addEventListener("click", () => { hideForm() });
+gameField.addEventListener("click", () => { addScore() });

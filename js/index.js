24d6e000c87e9eeb31482d7f1c88360e79159c68
killedmyself <markmisra@gmail.com>
2020-11-23 02:09:15 +0200

    Refactor index-js

--- js/index.js
+++ js/index.js
@@ -1,32 +1,35 @@
+// Buttons-section
 const startBtn = document.getElementById("Start");
 const newGameBtn = document.getElementById("NewGame");
 const saveBtn = document.getElementById("Save");
 
+// Point-section
 const points = document.getElementById("Points");
 const counter = points.querySelector(".counter");
 const finalScore = document.getElementById("Score");
 
+// Timer-section
 const clock = document.getElementById("Timer");
 const minutesSpan = clock.querySelector(".minutes");
 const secondsSpan = clock.querySelector(".seconds");
 
+// Overlay-section
 const overlay = document.getElementById("Hidden");
 const name = document.getElementById("Name");
 
+// Game field & Ranking
 const mainField = document.getElementById("GameField");
-
 const playersRanking = document.getElementById("Ranking");
 
 const myStorage = window.localStorage;
 
-const FIELD_SIZE = 5
+const _FIELD_SIZE = 5;
+const _SECONDS_TIME = 60;
 
-
-let timeinterval;
-let score = 0;
-let isStartGamePressed = false;
-let timeLimitExceeded = false;
-
+let _timeInterval;
+let _score = 0;
+let _isStartGamePressed = false;
+let _timeLimitExceeded = false;
 
 
 
@@ -34,29 +37,55 @@ const printDefaults = () => {
   minutesSpan.innerHTML = "01"
   secondsSpan.innerHTML = "00";
   counter.innerHTML = "<b>0</b>";
+  name.value = "";
 }
 
 const printScore = (id ,point) => {
   id.innerHTML = point.toString();
 }
 
-
 const reset = () => {
-  if (timeinterval) {
-    clearInterval(timeinterval);
-  }
-  printDefaults();
-  name.value = "";
-  score = 0;
-  isStartGamePressed = false;
+  
+  resetTimer();
+  resetEventListeners();
+  resetGameField();
 
+  printDefaults();
+  
+  _score = 0;
+  _isStartGamePressed = false;
 }
+
+const resetTimer = () => {
+    if (_timeInterval) {
+    clearInterval(_timeInterval);
+  }
+}
+
+const resetEventListeners = () => {
+  saveBtn.removeEventListener("click", () => { hideForm() });
+  mainField.removeEventListener("click", (element) => { 
+    element = element || window.event;
+    if(element.target.className=="center columns-wrapper-5"){
+      return;
+    }
+    gameLogic(element.target); 
+  });
+}
+
+const resetGameField = () => {
+
+  for (var i = 0; i < _gameFields.length; i++) {
+    let cube = document.getElementById(i); 
+    cube.style.backgroundColor = "#ffffff83";
+    cube.style.visibility = "visible";
+  }
+}
+
 
 /**
 *   Timer
 */
-
-
 const getTimeRemaining = endtime => {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
@@ -71,7 +100,7 @@ const initializeClock = (endtime) => {
   const updateClock = () => {
     var t = getTimeRemaining(endtime);
     if (t.minutes == 0 && t.seconds == 0) {
-      timeLimitExceeded = true;
+      _timeLimitExceeded = true;
       newGame()
     }
     minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
@@ -79,18 +108,19 @@ const initializeClock = (endtime) => {
   }
 
   updateClock();
-  timeinterval = setInterval(updateClock, 1000);
+  _timeInterval = setInterval(updateClock, 1000);
 }
 
+
 /**
-*   TODO: create new form to save the name
+*  Save-name form
 */ 
-
-
 const showForm = () => {
   overlay.style.display = 'grid';
+  saveBtn.addEventListener("click", () => { hideForm() });
 
-  printScore(finalScore, score)
+  printScore(finalScore, _score);
+  reset();
 }
 
 const hideForm = () => {
@@ -100,61 +130,66 @@ const hideForm = () => {
   updateList();
 }
 
+
 /**
 *   Buttons
 */
-
-
 const startGame = () => {
-  reset();
-  let deadline = new Date(Date.parse(new Date()) + 60 * 1000);
+  let deadline = new Date(Date.parse(new Date()) + _SECONDS_TIME * 1000);
   initializeClock(deadline);
-  isStartGamePressed = true;
+  _isStartGamePressed = true;
   updateGameFields()
+  mainField.addEventListener("click", (element) => { 
+    element = element || window.event;
+    if(element.target.className=="center columns-wrapper-5"){
+      return;
+    }
+    gameLogic(element.target); 
+  });
+
 }
 
 const newGame = () => {
-  if (score == 0 && isStartGamePressed == false) {
+  if (_score == 0 && _isStartGamePressed == false) {
     alert("Play this one - you won\'t regret it!");
     return;
   }
   showForm();
-
-  reset();
 }
 
 /**
-*   TODO: create game logic 
+*  Game logic 
 */  
-
-
 const addScore = () => {
-  if (!isStartGamePressed) {
+  if (!_isStartGamePressed) {
     return;
   }
-  score++;
-  printScore(counter,score);
+  _score++;
+  printScore(counter,_score);
+}
+
+const addNewBlocks = () => {
+  let randomId = Math.floor(Math.random() * _gameFields.length);
+  let randomAmount = Math.random() <= 0.4;
+
+  while (_gameFields[randomId].isActive != false){
+    randomId = Math.floor(Math.random() * _gameFields.length);
+  }
+
+  _gameFields[randomId].isActive = true;
+  document.getElementById(randomId).style.visibility = 'visible';
 }
 
 const gameLogic = target => {
-  gameFields[target.id].isActive = false;
+  if (!_isStartGamePressed) {
+    return;
+  }
+
+  _gameFields[target.id].isActive = false;
   target.style.visibility = 'hidden';
   addScore();
   addNewBlocks();
 }
-
-const addNewBlocks = () => {
-  let randomId = Math.floor(Math.random() * gameFields.length);
-  let randomAmount = Math.random() <= 0.4;
-
-  while (gameFields[randomId].isActive != false){
-    randomId = Math.floor(Math.random() * gameFields.length);
-  }
-
-  gameFields[randomId].isActive = true;
-  document.getElementById(randomId).style.visibility = 'visible';
-}
-
 
 const setRandomColor = () => {
   let color
@@ -186,7 +221,7 @@ const setRandomColor = () => {
   return color;
 }
 
-let gameFields = Array(25).fill().map( gameField=> {
+let _gameFields = Array(25).fill().map( gameField=> {
   return {
     color : setRandomColor(),
     isActive: Math.random() <= 0.1
@@ -195,23 +230,21 @@ let gameFields = Array(25).fill().map( gameField=> {
 
 const availableGameFields = (gameField) => { gameField.isActive == true ;}
 
-
 const printGameFields = () => {
   let ids = 0;
 
-  if (!gameFields.some(availableGameFields)) {
-      gameFields[Math.floor(Math.random() * gameFields.length)].isActive = true;   
+  if (!_gameFields.some(availableGameFields)) {
+      _gameFields[Math.floor(Math.random() * _gameFields.length)].isActive = true;   
   }
 
-  for (var i = 0; i < FIELD_SIZE; i++) {
+  for (var i = 0; i < _FIELD_SIZE; i++) {
     let wrapper = document.createElement('div');
     wrapper.className = "center columns-wrapper-5";
     mainField.appendChild(wrapper);
 
-    for (var j = 0; j < FIELD_SIZE; j++) {
+    for (var j = 0; j < _FIELD_SIZE; j++) {
       let newElement = document.createElement('div');
       newElement.className = "center neu-center neu-cube";
-      newElement.style.backgroundColor = gameFields[ids].color;
       newElement.id = ids;
       wrapper.appendChild(newElement);
       ids++;
@@ -221,21 +254,21 @@ const printGameFields = () => {
 
 const updateGameFields = () => {
 
-  for (var i = 0; i < gameFields.length; i++) {
-    if (!gameFields[i].isActive) {
-        document.getElementById(i).style.visibility = 'hidden';
+  for (var i = 0; i < _gameFields.length; i++) {
+
+    let cube = document.getElementById(i); 
+    cube.style.backgroundColor = _gameFields[i].color;
+    
+    if (!_gameFields[i].isActive) {
+        cube.style.visibility = 'hidden';
     }
   }
 }
 
 
-printGameFields();
-
 /**
 *   side-bar impl
 */
-
-
 const updateList = () => {
   if (Object.keys(localStorage).length === 0) {
     return;
@@ -267,24 +300,11 @@ const printListOfPlayers = () => {
     li.appendChild(document.createTextNode( rankingList[i]));
     playersRanking.appendChild(li);
   }
-
 }
 
+
+printGameFields();
 printListOfPlayers();
 
 startBtn.addEventListener("click", () => { startGame() });
 newGameBtn.addEventListener("click", () => { newGame() });
-saveBtn.addEventListener("click", () => { hideForm() });
-mainField.addEventListener("click", (element) => { 
-  element = element || window.event;
-  if(element.target.className=="center columns-wrapper-5"){
-    return;
-  }
-  gameLogic(element.target); 
-});
-
-
-
-//TODO: CREATE ENDGAME
-//REFACTOR JS
-// ADD STYLES
